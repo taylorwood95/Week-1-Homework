7c7650184f25fc9ece4863214cf7c75baa58cb5b
taylorwood95@hotmail.com <taylorwood95@hotmail.com>
2023-01-09 15:42:31 +0000

    First commit

---
+++ precourse_recap.py
@@ -0,0 +1,22 @@
+num1 = 5
+num2 = 10
+
+result = num1 * num2
+
+print(result)
+
+
+result2 = num1 + num2
+
+print(result2)
+
+
+string1 = 'Hello'
+string2 = ' World'
+
+print(string1 + string2)
+
+my_age = 27
+my_string = 'My age is '
+
+print(my_string + str(my_age))
